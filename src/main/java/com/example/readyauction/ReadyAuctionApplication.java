7728bdbe5ad88ed4352913c00fca58ef38b1bba1
minjiwon <mjo4260@naver.com>
2024-07-15 19:58:31 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/readyauction/ReadyAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.readyauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadyAuctionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReadyAuctionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/readyauction/ReadyAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readyauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadyAuctionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
