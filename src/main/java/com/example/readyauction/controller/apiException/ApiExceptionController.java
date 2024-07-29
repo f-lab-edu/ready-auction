15c158e67ffaadfb7c325e45354e2e6cff54410b
minjiwon <mjo4260@naver.com>
2024-07-30 02:02:42 +0900

    fix : @Valid ExceptionHandler 삭제

--- src/main/java/com/example/readyauction/controller/apiException/ApiExceptionController.java
+++ src/main/java/com/example/readyauction/controller/apiException/ApiExceptionController.java
@@ -1,43 +1,34 @@
-package com.example.readyauction.controller.user;
+package com.example.readyauction.controller.apiException;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.example.readyauction.controller.user.error.ErrorResponse;
+import com.example.readyauction.controller.apiException.error.ErrorResponse;
 import com.example.readyauction.exception.user.DuplicatedUserIdException;
 import com.example.readyauction.exception.user.LoginFailException;
 import com.example.readyauction.exception.user.NotFoundUserException;
 
 @RestControllerAdvice
-public class UserExceptionApiController {
-
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ErrorResponse validFailException(MethodArgumentNotValidException e) {
-		return new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
-			e.getBindingResult().getFieldError().getDefaultMessage());
-
-	}
+public class ApiExceptionController {
 
 	@ExceptionHandler(NotFoundUserException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public ErrorResponse notFoundUser(NotFoundUserException e) {
-		return new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+		return new ErrorResponse(e.getMessage());
 	}
 
 	@ExceptionHandler(DuplicatedUserIdException.class)
 	@ResponseStatus(HttpStatus.CONFLICT)
 	public ErrorResponse duplicatedUser(DuplicatedUserIdException e) {
-		return new ErrorResponse(HttpStatus.CONFLICT.value(), e.getMessage());
+		return new ErrorResponse(e.getMessage());
 	}
 
 	@ExceptionHandler(LoginFailException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ErrorResponse loginFail(LoginFailException e) {
-		return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+		return new ErrorResponse(e.getMessage());
 	}
 
 }
