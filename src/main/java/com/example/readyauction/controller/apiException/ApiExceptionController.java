5d813495f05dd4fc8d255fb0f3a7f3df7a73ea50
minjiwon <mjo4260@naver.com>
2024-08-14 01:56:39 +0900

    feat: 상품 API 예외 처리 추가

--- src/main/java/com/example/readyauction/controller/apiException/ApiExceptionController.java
+++ src/main/java/com/example/readyauction/controller/apiException/ApiExceptionController.java
@@ -1,34 +1,54 @@
 package com.example.readyauction.controller.apiException;
 
+import com.example.readyauction.controller.apiException.error.ErrorResponse;
+import com.example.readyauction.exception.product.NotFoundProductException;
+import com.example.readyauction.exception.product.ProductNotPendingException;
+import com.example.readyauction.exception.product.UnauthorizedProductAccessException;
+import com.example.readyauction.exception.user.DuplicatedUserIdException;
+import com.example.readyauction.exception.user.LoginFailException;
+import com.example.readyauction.exception.user.NotFoundUserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.example.readyauction.controller.apiException.error.ErrorResponse;
-import com.example.readyauction.exception.user.DuplicatedUserIdException;
-import com.example.readyauction.exception.user.LoginFailException;
-import com.example.readyauction.exception.user.NotFoundUserException;
-
 @RestControllerAdvice
 public class ApiExceptionController {
 
-	@ExceptionHandler(NotFoundUserException.class)
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public ErrorResponse notFoundUser(NotFoundUserException e) {
-		return new ErrorResponse(e.getMessage());
-	}
+    @ExceptionHandler(NotFoundUserException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse notFoundUser(NotFoundUserException e) {
+        return new ErrorResponse(e.getMessage());
+    }
 
-	@ExceptionHandler(DuplicatedUserIdException.class)
-	@ResponseStatus(HttpStatus.CONFLICT)
-	public ErrorResponse duplicatedUser(DuplicatedUserIdException e) {
-		return new ErrorResponse(e.getMessage());
-	}
+    @ExceptionHandler(DuplicatedUserIdException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse duplicatedUser(DuplicatedUserIdException e) {
+        return new ErrorResponse(e.getMessage());
+    }
 
-	@ExceptionHandler(LoginFailException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ErrorResponse loginFail(LoginFailException e) {
-		return new ErrorResponse(e.getMessage());
-	}
+    @ExceptionHandler(LoginFailException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse loginFail(LoginFailException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundProductException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse notFoundProduct(NotFoundProductException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(UnauthorizedProductAccessException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse productNotAccess(UnauthorizedProductAccessException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(ProductNotPendingException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse productStatusNotPending(ProductNotPendingException e) {
+        return new ErrorResponse(e.getMessage());
+    }
 
 }
