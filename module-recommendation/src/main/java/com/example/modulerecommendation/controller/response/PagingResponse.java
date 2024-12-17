03b1274ad7b6433fb6d3d536cb624a7e3c077a77
minjiwon <mjo4260@naver.com>
2024-12-18 02:47:42 +0900

    feat: 상품 추천 서버를 위한 ProductService 코드 추가

---
+++ module-recommendation/src/main/java/com/example/modulerecommendation/controller/response/PagingResponse.java
@@ -0,0 +1,27 @@
+package com.example.modulerecommendation.controller.response;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PagingResponse<T> {
+    private List<T> items;
+    private int pageNo; // 현재 페이지 넘버
+
+    @Builder
+    private PagingResponse(List<T> items, int pageNo) {
+        this.items = items;
+        this.pageNo = pageNo;
+    }
+
+    public static <T> PagingResponse<T> from(List<T> items, int pageNo) {
+        return PagingResponse.<T>builder()
+            .items(items)
+            .pageNo(pageNo)
+            .build();
+    }
+}
---
+++ module-recommendation/src/main/java/com/example/modulerecommendation/service/ProductListingService.java
@@ -0,0 +1,56 @@
+package com.example.modulerecommendation.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.moduledomain.domain.product.Product;
+import com.example.moduledomain.domain.product.ProductImage;
+import com.example.moduledomain.repository.product.ProductImageRepository;
+import com.example.moduledomain.repository.product.ProductRepository;
+import com.example.modulerecommendation.controller.response.ImageResponse;
+import com.example.modulerecommendation.controller.response.PagingResponse;
+import com.example.modulerecommendation.controller.response.ProductFindResponse;
+
+@Service
+public class ProductListingService {
+
+    private final ProductRepository productRepository;
+    private final ProductImageRepository productImageRepository;
+
+    public ProductListingService(ProductRepository productRepository, ProductImageRepository productImageRepository) {
+        this.productRepository = productRepository;
+        this.productImageRepository = productImageRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public PagingResponse<ProductFindResponse> findRecommendationProducts(List<Long> productIds, int pageNo) {
+        List<Product> products = findByIdIn(productIds);
+        List<ProductFindResponse> productFindResponses = products.stream()
+            .map(this::convertToProductFindResponse)
+            .collect(Collectors.toList());
+        return PagingResponse.from(productFindResponses, pageNo);
+
+    }
+
+    @Transactional(readOnly = true)
+    public List<Product> findByIdIn(List<Long> productIds) {
+        return productRepository.findByIdIn(productIds);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProductImage> getImage(Long productId) {
+        List<ProductImage> productImages = productImageRepository.findByProductId(productId);
+        return productImages;
+    }
+
+    private ProductFindResponse convertToProductFindResponse(Product product) {
+        List<ProductImage> productImages = getImage(product.getId());
+        List<ImageResponse> imageResponses = productImages.stream()
+            .map(ImageResponse::from)
+            .collect(Collectors.toList());
+        return ProductFindResponse.from(product, imageResponses);
+    }
+}
