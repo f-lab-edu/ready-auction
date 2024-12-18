3b22fe8fb8280339eabf3930ac60c8503d5a6baa
minjiwon <mjo4260@naver.com>
2024-12-18 20:53:06 +0900

    feat: 추천 서버 Spring Security 설정

---
+++ module-recommendation/src/main/java/com/example/modulerecommendation/config/CustomAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package com.example.modulerecommendation.config;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Collections;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.moduledomain.domain.user.User;
+import com.example.moduledomain.repository.user.UserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomAuthenticationFilter extends OncePerRequestFilter {
+
+    private final UserRepository userRepository;
+
+    public CustomAuthenticationFilter(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader("Authorization");
+        String token = authorizationHeader.substring(7);
+
+        // 토큰을 Base64 디코딩하여 userId:password 문자열 추출
+        String decodedToken = new String(Base64.getDecoder().decode(token));
+        String[] userCredentials = decodedToken.split(":");
+        String userId = userCredentials[0];
+        String password = userCredentials[1];
+
+        User user = userRepository.findByUserId(userId).orElse(null);
+        if (user == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        
+        if (password.equals(user.getEncodedPassword())) {
+            UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(userId, null,
+                    Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
