2a478663aa9e6c1b5440d01706ad7d128d11d8b9
minjiwon <mjo4260@naver.com>
2025-01-18 04:54:23 +0900

    Refactor: 좋아요 Redis Config 파일명 수정

--- module-api/src/main/java/com/example/moduleapi/config/LikeRedisConfig.java
+++ module-api/src/main/java/com/example/moduleapi/config/LikeRedisConfig.java
@@ -9,18 +9,12 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @Configuration
-public class RedisConfig {
+public class LikeRedisConfig {
     @Value("${spring.data.redis.like.host}")
     private String likeRedisHost;
 
     @Value("${spring.data.redis.like.port}")
     private int likeRedisPort;
-
-    @Value("${spring.data.redis.session.host}")
-    private String sessionRedisHost;
-
-    @Value("${spring.data.redis.session.port}")
-    private int sessionRedisPort;
 
     @Bean
     public RedisConnectionFactory likeRedisConnectionFactory() {
@@ -29,22 +23,6 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<Long, Long> likeRedisTemplate() {
-        RedisTemplate<Long, Long> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(likeRedisConnectionFactory());
-
-        // redis-cli을 통해 직접 데이터 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
-        redisTemplate.setKeySerializer(new GenericToStringSerializer<>(Long.class));
-        redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Long.class));
-        return redisTemplate;
-    }
-
-    @Bean
-    public RedisConnectionFactory sessionRedisConnectionFactory() {
-        return new LettuceConnectionFactory(sessionRedisHost, sessionRedisPort);
-    }
-
-    @Bean
-    public RedisTemplate<Long, Long> sessionRedisTemplate() {
         RedisTemplate<Long, Long> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(likeRedisConnectionFactory());
 
