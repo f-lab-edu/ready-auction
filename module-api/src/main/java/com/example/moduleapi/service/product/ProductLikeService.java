be90f719abde7affd81b7754a64e5b8a86d3edd0
minjiwon <mjo4260@naver.com>
2025-01-22 01:39:07 +0900

    Refactor: 불필요한 코드 및 주석 삭제

--- module-api/src/main/java/com/example/moduleapi/service/product/ProductLikeService.java
+++ module-api/src/main/java/com/example/moduleapi/service/product/ProductLikeService.java
@@ -1,18 +1,17 @@
 package com.example.moduleapi.service.product;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
+import com.example.moduleapi.controller.response.product.ProductLikeResponse;
+import com.example.moduledomain.domain.user.User;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.moduleapi.controller.response.product.ProductLikeResponse;
-import com.example.moduledomain.domain.user.User;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class ProductLikeService {
@@ -23,15 +22,10 @@ public class ProductLikeService {
         this.redisTemplate = redisTemplate;
     }
 
-    /**
-     * setOperations 1번째 : key = productId, value = 자료구조Set,  userId 저장 : 상품 좋아요 개수 계산 목적
-     * setOperations 2번째 : key = userId, value = 자료구조Set,  productId 저장 : 유저별 좋아요 누른 상품 조회 목적
-     */
     @Transactional
     public ProductLikeResponse addLike(User user, Long productId) {
         SetOperations<Long, Long> setOperations = redisTemplate.opsForSet();
         setOperations.add(productId, user.getId());
-        setOperations.add(user.getId(), productId);
         return new ProductLikeResponse(countProductLikesByProductId(productId));
     }
 
@@ -39,7 +33,6 @@ public class ProductLikeService {
     public ProductLikeResponse deleteLike(User user, Long productId) {
         SetOperations<Long, Long> setOperations = redisTemplate.opsForSet();
         setOperations.remove(productId, user.getId());
-        setOperations.remove(user.getId(), productId);
         return new ProductLikeResponse(countProductLikesByProductId(productId));
     }
 
@@ -50,8 +43,8 @@ public class ProductLikeService {
 
     @Transactional(readOnly = true)
     public List<Long> getUsersByProductId(Long productId) {
-        Set<Long> userIds = redisTemplate.opsForSet().members(productId);
-        // null이 아닌 빈 Set을 반환
+        Set<Long> userIds = redisTemplate.opsForSet()
+                                         .members(productId);
         if (userIds == null) {
             userIds = Collections.emptySet();
         }
@@ -60,13 +53,8 @@ public class ProductLikeService {
 
     @Transactional
     public int countProductLikesByProductId(Long productId) {
-        Long count = redisTemplate.opsForSet().size(productId);
+        Long count = redisTemplate.opsForSet()
+                                  .size(productId);
         return count == null ? 0 : count.intValue();
-    }
-
-    @Transactional
-    public Set<Long> getLikedProductListByUserId(Long userId) {
-        Set<Long> productIds = redisTemplate.opsForSet().members(userId);
-        return productIds;
     }
 }
