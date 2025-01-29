65325735e7a772fc2f50d7e4934d74d1e810e7d6
minjiwon <mjo4260@naver.com>
2025-01-29 23:08:15 +0900

    Refactor: Rest Docs 문서 최신화 및 테스트코드 한글 오타 수정

--- module-api/src/test/java/com/example/moduleapi/controller/auction/AuctionControllerTest.java
+++ module-api/src/test/java/com/example/moduleapi/controller/auction/AuctionControllerTest.java
@@ -56,29 +56,29 @@ class AuctionControllerTest {
                 eq(1L))).thenReturn(AuctionFixtures.가격_제안_응답);
 
         mockMvc.perform(post("/api/v1/auctions/{productId}", 1L)
-                       .contentType(MediaType.APPLICATION_JSON)
-                       .content(objectMapper.writeValueAsString(AuctionFixtures.가격_제안_요청)))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(AuctionFixtures.가격_제안_요청)))
                .andExpect(status().isOk())
                .andDo(document("경매 가격 제안",
-                       preprocessRequest(prettyPrint()),
-                       preprocessResponse(prettyPrint()),
-                       pathParameters(
-                               parameterWithName("productId").description("상품 ID")
-                       ),
-                       requestFields(
-                               fieldWithPath("biddingPrice").type(JsonFieldType.NUMBER)
-                                                            .description("입찰자가 제안한 가격")
-                       ),
-                       responseFields(
-                               fieldWithPath("productId").description("입찰 성공한 상품 ID"),
-                               fieldWithPath("rateOfIncrease").description("이전 최고가 대비 상승률")
-                       ))
+                               preprocessRequest(prettyPrint()),
+                               preprocessResponse(prettyPrint()),
+                               pathParameters(
+                                       parameterWithName("productId").description("상품 ID")
+                               ),
+                               requestFields(
+                                       fieldWithPath("biddingPrice").type(JsonFieldType.NUMBER)
+                                                                    .description("입찰자가 제안한 가격")
+                               ),
+                               responseFields(
+                                       fieldWithPath("productId").description("입찰 성공한 상품 ID"),
+                                       fieldWithPath("rateOfIncrease").description("이전 최고가 대비 상승률")
+                               ))
                );
 
     }
 
     @Test
-    @DisplayName("가격 입찰 API - 실채")
+    @DisplayName("가격 입찰 API - 실패")
     @WithMockCustomUser
     public void tenderFail() throws Exception {
         when(auctionService.biddingPrice(
@@ -87,23 +87,22 @@ class AuctionControllerTest {
                 eq(1L))).thenThrow(AuctionFixtures.가격_제안_실패_예외);
 
         mockMvc.perform(post("/api/v1/auctions/{productId}", 1L)
-                       .contentType(MediaType.APPLICATION_JSON)
-                       .content(objectMapper.writeValueAsString(AuctionFixtures.가격_제안_요청)))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(AuctionFixtures.가격_제안_요청)))
                .andExpect(status().isBadRequest())
                .andDo(document("경매 가격 제안 입찰 실패",
-                       preprocessRequest(prettyPrint()),
-                       preprocessResponse(prettyPrint()),
-                       pathParameters(
-                               parameterWithName("productId").description("상품 ID")
-                       ),
-                       requestFields(
-                               fieldWithPath("biddingPrice").type(JsonFieldType.NUMBER
-                                                            )
-                                                            .description("입찰자가 제안한 가격")
-                       ),
-                       responseFields(
-                               fieldWithPath("message").description("입찰 실패 오류 메시지")
-                       ))
+                               preprocessRequest(prettyPrint()),
+                               preprocessResponse(prettyPrint()),
+                               pathParameters(
+                                       parameterWithName("productId").description("상품 ID")
+                               ),
+                               requestFields(
+                                       fieldWithPath("biddingPrice").type(JsonFieldType.NUMBER)
+                                                                    .description("입찰자가 제안한 가격")
+                               ),
+                               responseFields(
+                                       fieldWithPath("message").description("입찰 실패 오류 메시지")
+                               ))
                );
 
     }
@@ -118,22 +117,22 @@ class AuctionControllerTest {
                 eq(1L))).thenThrow(AuctionFixtures.레디스_분산락_획득실패_예외);
 
         mockMvc.perform(post("/api/v1/auctions/{productId}", 1L)
-                       .contentType(MediaType.APPLICATION_JSON)
-                       .content(objectMapper.writeValueAsString(AuctionFixtures.가격_제안_요청)))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(AuctionFixtures.가격_제안_요청)))
                .andExpect(status().is5xxServerError())
                .andDo(document("경매 가격 제안 실패 - 분산락 획득",
-                       preprocessRequest(prettyPrint()),
-                       preprocessResponse(prettyPrint()),
-                       pathParameters(
-                               parameterWithName("productId").description("상품 ID")
-                       ),
-                       requestFields(
-                               fieldWithPath("biddingPrice").type(JsonFieldType.NUMBER)
-                                                            .description("입찰자가 제안한 가격")
-                       ),
-                       responseFields(
-                               fieldWithPath("message").description("레디스 분산락 획득 실패 오류 메시지")
-                       ))
+                               preprocessRequest(prettyPrint()),
+                               preprocessResponse(prettyPrint()),
+                               pathParameters(
+                                       parameterWithName("productId").description("상품 ID")
+                               ),
+                               requestFields(
+                                       fieldWithPath("biddingPrice").type(JsonFieldType.NUMBER)
+                                                                    .description("입찰자가 제안한 가격")
+                               ),
+                               responseFields(
+                                       fieldWithPath("message").description("레디스 분산락 획득 실패 오류 메시지")
+                               ))
                );
     }
 
@@ -147,22 +146,22 @@ class AuctionControllerTest {
                 eq(1L))).thenThrow(AuctionFixtures.레디스_분산락_처리과정_예외);
 
         mockMvc.perform(post("/api/v1/auctions/{productId}", 1L)
-                       .contentType(MediaType.APPLICATION_JSON)
-                       .content(objectMapper.writeValueAsString(AuctionFixtures.가격_제안_요청)))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(AuctionFixtures.가격_제안_요청)))
                .andExpect(status().is5xxServerError())
                .andDo(document("경매 가격 제안 실패 - 분산락 처리과정",
-                       preprocessRequest(prettyPrint()),
-                       preprocessResponse(prettyPrint()),
-                       pathParameters(
-                               parameterWithName("productId").description("상품 ID")
-                       ),
-                       requestFields(
-                               fieldWithPath("biddingPrice").type(JsonFieldType.NUMBER)
-                                                            .description("입찰자가 제안한 가격")
-                       ),
-                       responseFields(
-                               fieldWithPath("message").description("레디스 분산락 처리과정 실패 오류 메시지")
-                       ))
+                               preprocessRequest(prettyPrint()),
+                               preprocessResponse(prettyPrint()),
+                               pathParameters(
+                                       parameterWithName("productId").description("상품 ID")
+                               ),
+                               requestFields(
+                                       fieldWithPath("biddingPrice").type(JsonFieldType.NUMBER)
+                                                                    .description("입찰자가 제안한 가격")
+                               ),
+                               responseFields(
+                                       fieldWithPath("message").description("레디스 분산락 처리과정 실패 오류 메시지")
+                               ))
                );
     }
 }
