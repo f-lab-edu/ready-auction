7c525e360e9da7a328da4cc40e69b483ee3c7f6e
minjiwon <mjo4260@naver.com>
2025-01-29 22:32:39 +0900

    Refactor: domain 모듈 불필요한 파일 삭제 - domain 모듈의 src/main 파일 삭제 - @EnableJpaRepositories와 @EntityScan으로 JPA 리포지토리 및 엔티티 스캔 설정

--- module-batch/src/main/java/com/example/modulebatch/ModuleBatchApplication.java
+++ module-batch/src/main/java/com/example/modulebatch/ModuleBatchApplication.java
@@ -3,11 +3,18 @@ package com.example.modulebatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.example.modulebatch", "com.example.moduledomain", "com.example.moduleapi.service", "com.example.moduleapi.config"})
+@SpringBootApplication(scanBasePackages = {
+        "com.example.modulebatch",
+        "com.example.moduledomain",
+        "com.example.moduleapi.service",
+        "com.example.moduleapi.config"
+})
+@EnableJpaRepositories(basePackages = "com.example.moduledomain")
+@EntityScan(basePackages = "com.example.moduledomain")
 public class ModuleBatchApplication {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(ModuleBatchApplication.class);
--- module-recommendation/src/main/java/com/example/modulerecommendation/ModuleRecommendationApplication.java
+++ module-recommendation/src/main/java/com/example/modulerecommendation/ModuleRecommendationApplication.java
@@ -2,10 +2,14 @@ package com.example.modulerecommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = {"com.example.*"})
 @EnableScheduling
+@SpringBootApplication(scanBasePackages = {"com.example.moduledomain"})
+@EnableJpaRepositories(basePackages = "com.example.moduledomain")
+@EntityScan(basePackages = "com.example.moduledomain")
 public class ModuleRecommendationApplication {
 
     public static void main(String[] args) {
